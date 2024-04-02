200ff6eea78fc11392fa2ea6768253eb7732cde2
Vincent Sit <vincentsit00@ymail.com>
2024-04-02 23:28:03 +1300

    Implement random enemy generation on load

--- src/Main.cs
+++ src/Main.cs
@@ -1,6 +1,25 @@
+using System;
 using Godot;
 
 public class Main : Node2D
 {
+	public override void _Ready()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			GenerateEnemy();
+		}
+	}
 
+	public void GenerateEnemy()
+	{
+		var screenSize = GetViewport().GetVisibleRect().Size;
+		Random random = new Random();
+		var enemy = (Node2D) ResourceLoader.Load<PackedScene>("res://src//features//entities//enemy//Enemy.tscn").Instance();
+		AddChild(enemy);
+		Vector2 position = Vector2.Zero;
+		position.x = random.Next(0, (int) screenSize.x);
+		position.y = random.Next(0, (int) screenSize.y);
+		enemy.GlobalPosition = position;
+	}
 }
--- src/features/entities/enemy/Enemy.cs
+++ src/features/entities/enemy/Enemy.cs
@@ -3,8 +3,6 @@ using Godot;
 public class Enemy : KinematicBody2D
 {
 	public int Speed { get; set; } = 300;
-	public Vector2 MovementDirection { get; set; }
-	public MovementControl MovementControl { get; set; }
 	public Allegiance Allegiance { get; set; } = Allegiance.ENEMY;
 
 	public override void _Process(float delta)
